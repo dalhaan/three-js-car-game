b714a5a41eab892297ba17829a93e93fcb877ad4
Dallan Freemantle <dallan@alphero.com>
2022-11-27 12:47:20 +1300

    Add ground

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -5,6 +5,7 @@ import {
   PerspectiveCamera,
 } from "@react-three/drei";
 import { Track } from "./Track";
+import { Ground } from "./Ground";
 
 export function Scene() {
   return (
@@ -15,6 +16,7 @@ export function Scene() {
       <OrbitControls target={[-2.64, -0.71, 0.03]} />
 
       <Track />
+      <Ground />
     </Suspense>
   );
 }
